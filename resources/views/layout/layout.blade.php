cc6e6389e5b518091c8af0bfe5d7cd9d5abf4a84
Sebastián Campos <blowunex@hotmail.com>
2020-09-01 18:50:51 -0600

    1.0.4

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,11 +1,15 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 <link rel="stylesheet" href="{{ asset('css/postContenido.css') }}">
+
+<script src="{{ asset('js/app.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/all.css') }}">
+
 
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 <script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
 
 <script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
